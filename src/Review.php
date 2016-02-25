846d99b1d2967e5f7426bc74a66f50ebac5d92e0
Adam Ross Russell and Mary Warrington <marywarrington@gmail.com>
2016-02-25 09:27:01 -0800

    add save() and getAll() for the review class

--- src/Review.php
+++ src/Review.php
@@ -39,67 +39,67 @@
         }
 
 
-        // function save() //saves to database CHOMP
-        // {
-        //     $GLOBALS['DB']->exec("INSERT INTO restaurant (reviewer_name, review_score, review_content, restaurant_id) VALUES ('{$this->getRestName()}', '{$this->getLocation()}', '{$this->getPriceRange()}', {$this->getCuisineId()});");
-        //     $this->id = $GLOBALS['DB']->lastInsertId();
-        // }
+        function save() //saves to database CHOMP
+        {
+            $GLOBALS['DB']->exec("INSERT INTO review (reviewer_name, review_score, review_content, restaurant_id) VALUES ('{$this->getReviewerName()}', '{$this->getReviewScore()}', '{$this->getReviewContent()}', {$this->getRestaurantId()});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
         //
-        // static function getAll()
-        // {
-        //     $returned_restaurants = $GLOBALS['DB']->query("SELECT * FROM restaurant;");
-        //     $restaurants = [];
-        //
-        //     foreach ($returned_restaurants as $restaurant) {
-        //         $reviewer_name = $restaurant['reviewer_name'];
-        //         $id = $restaurant['id']; //?
-        //         $review_score = $restaurant['review_score'];
-        //         $review_content = $restaurant['review_content'];
-        //         $restaurant_id = $restaurant['restaurant_id'];
-        //         $new_restaurant = new Review($reviewer_name, $id, $review_score, $review_content, $restaurant_id);
-        //         array_push($restaurants, $new_restaurant);
-        //     }
-        //     return $restaurants;
-        // }
+        static function getAll()
+        {
+            $returned_reviews = $GLOBALS['DB']->query("SELECT * FROM review;");
+            $reviews = [];
+
+            foreach ($returned_reviews as $review) {
+                $reviewer_name = $review['reviewer_name'];
+                $id = $review['id']; //?
+                $review_score = $review['review_score'];
+                $review_content = $review['review_content'];
+                $restaurant_id = $review['restaurant_id'];
+                $new_review = new Review($reviewer_name, $review_score, $review_content, $restaurant_id, $id);
+                array_push($reviews, $new_review);
+            }
+            return $reviews;
+        }
         //
         // function updateRestName($new_reviewer_name)
         // {
-        //     $GLOBALS['DB']->exec("UPDATE restaurant SET reviewer_name = '{$new_reviewer_name}' WHERE id = {$this->getId()};");
+        //     $GLOBALS['DB']->exec("UPDATE review SET reviewer_name = '{$new_reviewer_name}' WHERE id = {$this->getId()};");
         //     $this->setRestName($new_reviewer_name);
         // }
         //
         // function updatePriceRange($new_review_content)
         // {
-        //     $GLOBALS['DB']->exec("UPDATE restaurant SET review_content = '{$new_review_content}' WHERE id = {$this->getId()};");
+        //     $GLOBALS['DB']->exec("UPDATE review SET review_content = '{$new_review_content}' WHERE id = {$this->getId()};");
         //     $this->setPriceRange($new_review_content);
         // }
         //
         // function updateLocation($new_review_score)
         // {
-        //     $GLOBALS['DB']->exec("UPDATE restaurant SET review_score = '{$new_review_score}' WHERE id = {$this->getId()};");
+        //     $GLOBALS['DB']->exec("UPDATE review SET review_score = '{$new_review_score}' WHERE id = {$this->getId()};");
         //     $this->setLocation($new_review_score);
         // }
         //
         // static function find($search_id)
         // {
-        //     $found_restaurant = null;
-        //     $restaurants = Review::getAll();
+        //     $found_review = null;
+        //     $reviews = Review::getAll();
         //
-        //     foreach ($restaurants as $restaurant) {
-        //         if ($restaurant->getId() == $search_id) {
-        //             $found_restaurant = $restaurant;
+        //     foreach ($reviews as $review) {
+        //         if ($review->getId() == $search_id) {
+        //             $found_review = $review;
         //         }
         //     }
-        //     return $found_restaurant;
+        //     return $found_review;
         // }
         //
-        // static function deleteAll(){
-        //     $GLOBALS['DB']->exec("DELETE FROM restaurant");
-        // }
+        static function deleteAll(){
+            $GLOBALS['DB']->exec("DELETE FROM review");
+        }
         //
         // function deleteOneReview()
         // {
-        //     $GLOBALS['DB']->exec("DELETE FROM restaurant WHERE id = {$this->getId()};");
+        //     $GLOBALS['DB']->exec("DELETE FROM review WHERE id = {$this->getId()};");
         // }
     }
 
--- tests/ReviewTest.php
+++ tests/ReviewTest.php
@@ -24,148 +24,101 @@
             Review::deleteAll();
         }
 
-        // function test_getName()
-        // {
-        //     $rest_name = "Bear Tooth";
-        //     $id = 1;
-        //     $location = "Spenard Road";
-        //     $price_range = "$";
-        //     $cuisine_id = 1;
-        //
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
-        //
-        //     //Act
-        //     $result = $test_restaurant->getRestName();
-        //
-        //     //Assert
-        //     $this->assertEquals($rest_name, $result);
-        // }
-        //
-        // function test_getId()
-        // {
-        //     //Arrange
-        //     $name = "Ethiopian";
-        //     $id = null;
-        //     $test_cuisine = new Cuisine($name, $id);
-        //     $test_cuisine->save();
-        //
-        //     $rest_name = "Bear Tooth";
-        //
-        //     $location = "Spenard Road";
-        //     $price_range = "$";
-        //     $cuisine_id = $test_cuisine->getId();
-        //     // var_dump($cuisine_id);
-        //
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
-        //     $test_restaurant->save();
-        //
-        //     //Act
-        //     $result = $test_restaurant->getId();
-        //
-        //     //Assert
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
-        // function test_getCuisineId()
-        // {
-        //     //Arrange
-        //     $name = "Mexican";
-        //     $id = null;
-        //     $test_cuisine = new Cuisine($name, $id);
-        //     $test_cuisine->save();
-        //
-        //     $rest_name = "La Bonita";
-        //     $cuisine_id = $test_cuisine->getId();
-        //     $location = "N Killingsworth";
-        //     $price_range = "$";
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
-        //
-        //     //Act
-        //     $result = $test_restaurant->getCuisineId();
-        //
-        //     //Assert
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
-        // function test_save()
-        // {
-        //     //Arrange
-        //     $rest_name = "Bear Tooth";
-        //     $id = 1;
-        //     $location = "Spenard Road";
-        //     $price_range = "$";
-        //     $cuisine_id = 1;
-        //
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
-        //
-        //     $test_restaurant->save();
-        //
-        //     //Act
-        //     $result = Restaurant::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals($test_restaurant, $result[0]);
-        // }
-        //
-        //
-        // function test_getAll()
-        // {
-        //     //Arrange
-        //     $rest_name = "Bear Tooth";
-        //     $id = 1;
-        //     $location = "Spenard Road";
-        //     $price_range = "$";
-        //     $cuisine_id = 1;
-        //
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
-        //
-        //     $test_restaurant->save();
-        //
-        //     $rest_name2 = "Organic Oasis";
-        //     $id = 2;
-        //     $location2 = "2606 Spenard Road";
-        //     $price_range2 = "$$";
-        //     $cuisine_id = 1;
-        //
-        //     $test_restaurant2 = new Restaurant($rest_name2, $id, $location2, $price_range2, $cuisine_id);
-        //
-        //     $test_restaurant2->save();
-        //
-        //     //Act
-        //     $result = Restaurant::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_restaurant, $test_restaurant2], $result);
-        // }
-        //
+        function test_getReviewerName()
+        {
+            $reviewer_name = "Adam";
+            $id = 1;
+            $review_score = 4;
+            $review_content = "This place was great!";
+            $restaurant_id = 1;
+
+            $test_review = new Review($reviewer_name, $review_score, $review_content, $restaurant_id, $id);
+
+            //Act
+            $result = $test_review->getReviewerName();
+
+            //Assert
+            $this->assertEquals($reviewer_name, $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $reviewer_name = "Adam";
+            $id = 1;
+            $review_score = 4;
+            $review_content = "This place was great!";
+            $restaurant_id = 1;
+
+            $test_review = new Review($reviewer_name, $review_score, $review_content, $restaurant_id, $id);
+
+            $test_review->save();
+
+            //Act
+            $result = Review::getAll();
+
+            //Assert
+            $this->assertEquals($test_review, $result[0]);
+        }
+
+
+        function test_getAll()
+        {
+            //Arrange
+            $id = null;
+            $reviewer_name = "Adam";
+            $review_score = 4;
+            $review_content = "This place was great!";
+            $restaurant_id = 1;
+
+            $test_review = new Review($reviewer_name, $review_score, $review_content, $restaurant_id, $id);
+
+            $test_review->save();
+
+            $reviewer_name2 = "Pete";
+            $review_score2 = 3;
+            $review_content2 = "SO EXPENSIVE";
+            $restaurant_id = 1;
+
+            $test_review2 = new Review($reviewer_name2, $review_score2, $review_content2, $restaurant_id, $id);
+
+            $test_review2->save();
+
+            //Act
+            $result = Review::getAll();
+
+            //Assert
+            $this->assertEquals([$test_review, $test_review2], $result);
+        }
+
         // function test_find()
         // {
         //     //Arrange
-        //     $rest_name = "Bear Tooth";
+        //     $reviewer_name = "Bear Tooth";
         //     $id = 1;
-        //     $location = "Spenard Road";
-        //     $price_range = "$";
-        //     $cuisine_id = 1;
+        //     $review_score = "Spenard Road";
+        //     $review_content = "$";
+        //     $restaurant_id = 1;
         //
-        //     $test_restaurant = new Restaurant($rest_name, $id, $location, $price_range, $cuisine_id);
+        //     $test_review = new Review($reviewer_name, $id, $review_score, $review_content, $restaurant_id);
         //
-        //     $test_restaurant->save();
+        //     $test_review->save();
         //
-        //     $rest_name2 = "Organic Oasis";
+        //     $reviewer_name2 = "Organic Oasis";
         //     $id = 2;
-        //     $location2 = "2606 Spenard Road";
-        //     $price_range2 = "$$";
-        //     $cuisine_id = 1;
+        //     $review_score2 = "2606 Spenard Road";
+        //     $review_content2 = "$$";
+        //     $restaurant_id = 1;
         //
-        //     $test_restaurant2 = new Restaurant($rest_name2, $id, $location2, $price_range2, $cuisine_id);
+        //     $test_review2 = new Review($reviewer_name2, $id, $review_score2, $review_content2, $restaurant_id);
         //
-        //     $test_restaurant2->save();
+        //     $test_review2->save();
         //
         //     //Act
-        //     $result = Restaurant::find($test_restaurant2->getId());
+        //     $result = Review::find($test_review2->getId());
         //
         //     //Assert
-        //     $this->assertEquals($test_restaurant2, $result);
+        //     $this->assertEquals($test_review2, $result);
         //
         // }
         //
